a3c9eb69e3bf39bfc76675a8d04ef6646272a0c6
Bart Vermeersch <vermeersch@gmail.com>
2020-05-26 13:54:40 +0200

    add O365 modules

--- configure-posh/Setup-PowerShellEnvironment.ps1
+++ configure-posh/Setup-PowerShellEnvironment.ps1
@@ -1,3 +1,8 @@
 Install-Module posh-git -Force
 Install-Module oh-my-posh -Force
 Install-Module posh-docker -Force
+
+
+Install-Module MSOnline
+Install-Module ExchangeOnlineManagement
+Install-Module AzureAD
