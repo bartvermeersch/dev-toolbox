163cbf428c03c4e5a67b84611bc2258a28c2b971
Bart Vermeersch <vermeersch@gmail.com>
2020-06-28 14:29:11 +0200

    Cleanup and add categories

--- apps/Setup-DevToolbox.ps1
+++ apps/Setup-DevToolbox.ps1
@@ -2,59 +2,69 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco upgrade chocolatey
 
-# Install my applications
-choco install az.powershell
-choco install microsoft-windows-terminal
-choco install 7zip -y
-choco install adobereader -y
-choco install microsoft-teams -y
-choco install notepadplusplus -y
-choco install vscode -y
+# Apps
+choco install Office365ProPlus -y
+#choco install microsoft-teams -y
+#choco install microsoft-teams.install -y
+choco install microsoft-edge-insider -y
 choco install googlechrome -y
 #choco install spotify -y # Invalid checksum, override with --ignore-checksums if you want to
+
+# Azure stuff
+choco install az.powershell -y
+choco install azure-cli -y
+choco install microsoftazurestorageexplorer -y
+choco install azure-data-studio -y
+choco install azure-cli -y
+
+# Utils
+choco install 7zip -y
+choco install adobereader -y
+choco install setpoint -y
+choco install notepadplusplus -y
+choco install paint.net -y
+choco install screentogif -y
+choco install irfanview -y
+choco install notepadplusplus -y
+choco install vlc -y
+choco install powertoys -y
+choco install grammarly -y
+
+# Dev
+choco install vscode -y
 choco install vscode-settingssync
 #choco install vscode-icons -y
 #choco install vscode-csharp -y
 #choco install vscode-powershell -y
 #choco install vscode-azurerm-tools -y
 #choco install vscode-markdownlint -y
-choco install microsoftazurestorageexplorer -y
-choco install powershell-core
-choco install azure-cli -y
 choco install git -y
 choco install postman -y
 choco install fiddler -y
-choco install grammarly -y
+choco install microsoft-windows-terminal -y
+choco install powershell-core -y
+choco install dotnetcore-sdk -y
+choco install nodejs-lts -y
+choco install rsat -y
+choco install armclient -y
+
+
+#Security
+choco install lastpass -y
+choco install keepassxc -y
 choco install keepass -y
 choco install keepass-plugin-keeanywhere -y
-choco install paint.net -y
-choco install armclient -y
-choco install screentogif -y
-
-
-choco install azure-data-studio
-choco install eid-belgium
-choco install eid-belgium-viewer
-choco install setpoint
-choco install dotnetcore-sdk
-choco install microsoft-edge-insider
-choco install notepadplusplus
-choco install vlc
-choco install powertoys
-choco install lastpass
-choco install nodejs-lts
-choco install microsoft-teams.install
-choco install rsat
-choco install OpenSSL.Light
-choco install microsoft-windows-terminal
-
-#To check
-# brother mfl pro suite MFC-9330CDW
+choco install keepass-plugin-rdp
+choco install OpenSSL.Light -y
+choco install eid-belgium -y
+choco install eid-belgium-viewer -y
 # cisco anyconnect secure mobile client
+# choco install openconnect-gui -y
 # citrx workspace
 # forticlient VPN
 
-# office 365 proplus
+#To check
+# brother mfl pro suite MFC-9330CDW
 
 # Show installed packages
 choco list --local-only
